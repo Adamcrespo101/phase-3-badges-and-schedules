6d9737fb4def4930e87d46d1168ae7d2d5cd3f73
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-05 09:38:20 -0400

    commit tree 515e067ea2e83c6e5274af8f2c82e9f24447808d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker name
+     "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names 
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms names
+    names.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |name_badge|
+        puts name_badge
+    end
+    assign_rooms(attendees).each do |room|
+        puts room
+
+    end
+end
+
+
+
